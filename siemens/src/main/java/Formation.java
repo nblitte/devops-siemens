d97672366a861a2bee08e6e393f8d891465121ff
Nathalie Blitte <nblitte@bitoo.fr>
2023-03-03 09:34:13 +0100

    Ajout projet siemens

---
+++ siemens/src/main/java/Formation.java
@@ -0,0 +1,13 @@
+package formation.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class Formation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
